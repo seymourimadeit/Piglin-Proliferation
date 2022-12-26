e563ad18f28bf81bd9f4eaa74f6c25f438ae7bec
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2022-12-26 13:12:55 -0800

    Update NoteBlockMixin.java

--- src/main/java/tallestred/piglinproliferation/mixins/NoteBlockMixin.java
+++ src/main/java/tallestred/piglinproliferation/mixins/NoteBlockMixin.java
@@ -3,6 +3,7 @@ package tallestred.piglinproliferation.mixins;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
@@ -34,7 +35,7 @@ public abstract class NoteBlockMixin extends Block {
 
     @Inject(method = "playNote", at = @At("TAIL"), cancellable = true)
     private void playNote(Entity p_261664_, BlockState p_261606_, Level p_261819_, BlockPos p_262042_, CallbackInfo ci) {
-        if (p_261819_.getBlockState(p_262042_.above()).is(PPBlocks.PIGLIN_ALCHEMIST_HEAD.get()) || p_261819_.getBlockState(p_262042_.above()).is(PPBlocks.PIGLIN_BRUTE_HEAD.get()) || p_261819_.getBlockState(p_262042_.above()).is(PPBlocks.ZOMBIFIED_PIGLIN_HEAD.get())) {
+        if ((p_261819_.getBlockState(p_262042_.above()).is(PPBlocks.PIGLIN_ALCHEMIST_HEAD.get()) || p_261819_.getBlockState(p_262042_.above()).is(PPBlocks.PIGLIN_BRUTE_HEAD.get()) || p_261819_.getBlockState(p_262042_.above()).is(PPBlocks.ZOMBIFIED_PIGLIN_HEAD.get())) && p_261819_.enabledFeatures().contains(FeatureFlags.UPDATE_1_20)) {
             p_261819_.blockEvent(p_262042_, this, 0, 0);
             p_261819_.gameEvent(p_261664_, GameEvent.NOTE_BLOCK_PLAY, p_262042_); // Chicanary
         }
