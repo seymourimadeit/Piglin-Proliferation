e71afbbc74f3db95f9f43a0a4cbadb86fafd1ddb
seymourimadeit <anemailaccount@mail.com>
2024-03-24 20:59:10 -0700

    w

---
+++ src/main/java/tallestred/piglinproliferation/mixins/NetherForestVegetationFeatureMixin.java
@@ -0,0 +1,30 @@
+package tallestred.piglinproliferation.mixins;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.HugeFungusConfiguration;
+import net.minecraft.world.level.levelgen.feature.NetherForestVegetationFeature;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import tallestred.piglinproliferation.common.PPTags;
+
+@Mixin(NetherForestVegetationFeature.class)
+public abstract class NetherForestVegetationFeatureMixin extends Feature {
+    public NetherForestVegetationFeatureMixin(Codec pCodec) {
+        super(pCodec);
+    }
+
+    @Inject(at = @At(value = "HEAD"), method = "place", cancellable = true)
+    protected void place(FeaturePlaceContext<HugeFungusConfiguration> pContext, CallbackInfoReturnable<Boolean> callbackInfoReturnable) {
+        WorldGenLevel worldgenlevel = pContext.level();
+        BlockPos pos = pContext.origin();
+        if (worldgenlevel.getLevel().structureManager().getStructureWithPieceAt(pos, PPTags.TRAVELLER_BASES).isValid()) {
+            callbackInfoReturnable.setReturnValue(false);
+        }
+    }
+}
