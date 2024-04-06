f72198e91510f2d828e8168e1a0bbad8cfd06848
Abigail Flowers <thatvaliantvole@gmail.com>
2024-04-06 17:15:16 +0100

    Made piglin traveller neutral unless you attack other piglins

--- src/main/java/tallestred/piglinproliferation/common/entities/ai/PiglinTravellerAi.java
+++ src/main/java/tallestred/piglinproliferation/common/entities/ai/PiglinTravellerAi.java
@@ -22,7 +22,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.schedule.Activity;
 import net.minecraft.world.item.CrossbowItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
@@ -200,7 +199,7 @@ public class PiglinTravellerAi extends PiglinAi {
             if (optional.isPresent() && Sensor.isEntityAttackableIgnoringLineOfSight(piglin, (LivingEntity) optional.get())) {
                 return optional;
             } else {
-                Optional optional3;
+                Optional<? extends LivingEntity> optional3;
                 if (brain.hasMemoryValue(MemoryModuleType.UNIVERSAL_ANGER)) {
                     optional3 = brain.getMemory(MemoryModuleType.NEAREST_VISIBLE_ATTACKABLE_PLAYER);
                     if (optional3.isPresent()) {
@@ -211,9 +210,6 @@ public class PiglinTravellerAi extends PiglinAi {
                 optional3 = brain.getMemory(MemoryModuleType.NEAREST_VISIBLE_NEMESIS);
                 if (optional3.isPresent()) {
                     return optional3;
-                } else if (!piglin.level().getBiome(piglin.getOnPos()).is(Biomes.WARPED_FOREST)){
-                    Optional<Player> optional2 = brain.getMemory(MemoryModuleType.NEAREST_TARGETABLE_PLAYER_NOT_WEARING_GOLD);
-                    return optional2.isPresent() && Sensor.isEntityAttackable(piglin, (LivingEntity) optional2.get()) ? optional2 : Optional.empty();
                 }
             }
         }
